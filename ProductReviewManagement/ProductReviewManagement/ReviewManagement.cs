6e6e148f4f051859f54f341bbcaaa0b179c0ca4c
Suraj-Dal <dsurajdal10@gmail.com>
2022-06-27 12:08:00 +0530

    [Suraj]: Usecase 1 to create product review list to store and retrieve default values is added.

---
+++ ProductReviewManagement/ProductReviewManagement/ReviewManagement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    internal class ReviewManagement
+    {
+        public void getReview(List<ProductReview> productReviews)
+        {
+            foreach (var item in productReviews)
+            {
+                Console.WriteLine("Product ID: "+ item.ProductId + "\tUser ID: " + item.UserId + "\tRating: " + item.Rating + "\tReview: " + item.Review + "\tisLike: " + item.isLike);
+            }
+        }
+    }
+}
